f5205ae2b700cfe366f3a04d0a1e8082d73b8d68
123al <123al@10.0.0.238>
2018-11-20 16:45:27 -0700

    This program displays the corresponding unicode of an inputed character.

---
+++ src/AlannaBotscharowChapter5pr2/Chapter5Program2.java
@@ -0,0 +1,565 @@
+package AlannaBotscharowChapter5pr2;
+import java.util.Scanner; 
+/* 
+ * Alanna Botscharow
+ * 11/18
+ */
+public class Chapter5Program2 {
+
+	public static void main(String[] args) {
+		
+				Scanner input = new Scanner(System.in);
+				
+				// Prompt user for input of a character. 
+				
+					System.out.println("Please enter a character: ");
+				
+				//Get user input
+					//declare variables 
+				
+				char ch = input.next().charAt(0); 
+				
+				//If statements
+					//Display 
+
+				//0-9
+				if (ch == '0') {
+					System.out.println("The unicode for the character 0 is \\u0030"); 
+					
+				}
+				else if (ch == '1') {
+					System.out.println("The unicode for the character 1 is \\u0031"); 
+				}
+				else if (ch == '2') {
+					System.out.println("The unicode for the character 2 is \\u0031"); 
+				}
+				else if (ch == '3') {
+					System.out.println("The unicode for the character 3 is \\u0032"); 
+				}
+				else if (ch == '4') {
+					System.out.println("The unicode for the character 4 is \\u0034"); 
+				}
+				else if (ch == '5') {
+					System.out.println("The unicode for the character 5 is \\u0035"); 
+				}
+				else if (ch == '6') {
+					System.out.println("The unicode for the character 6 is \\u0036"); 
+				}
+				else if (ch == '7') {
+					System.out.println("The unicode for the character 7 is \\u0037"); 
+				}
+				else if (ch == '8') {
+					System.out.println("The unicode for the character 8 is \\u0038"); 
+					
+				}
+				
+				//a-z
+
+				
+
+				
+
+				else if (ch == 'a') {
+
+					System.out.println("The unicode for the character a is \\u0061"); 
+
+				}
+
+				else if (ch == 'b') {
+
+					System.out.println("The unicode for the character b is \\u0062"); 
+
+				}
+
+				else if (ch == 'c') {
+
+					System.out.println("The unicode for the character c is \\u0063"); 
+
+				}
+
+				else if (ch == 'd') {
+
+					System.out.println("The unicode for the character d is \\u0064"); 
+
+				}
+
+				else if (ch == 'e') {
+
+					System.out.println("The unicode for the character e is \\u0065"); 
+
+				}
+
+				else if (ch == 'f') {
+
+					System.out.println("The unicode for the character f is \\u0066"); 
+
+				}
+
+				else if (ch == 'g') {
+
+					System.out.println("The unicode for the character g is \\u0067"); 
+
+				}
+
+				else if (ch == 'h') {
+
+					System.out.println("The unicode for the character h is \\u0068"); 
+
+				}
+
+				else if (ch == 'i') {
+
+					System.out.println("The unicode for the character i is \\u0069"); 
+
+				}
+
+				else if (ch == 'j') {
+
+					System.out.println("The unicode for the character j is \\u006A"); 
+
+				}
+
+				else if (ch == 'k') {
+
+					System.out.println("The unicode for the character k is \\u006B"); 
+
+				}
+
+				else if (ch == 'l') {
+
+					System.out.println("The unicode for the character l is \\u006C"); 
+
+				}
+
+				else if (ch == 'm') {
+
+					System.out.println("The unicode for the character m is \\u006D"); 
+
+				}
+
+				else if (ch == 'n') {
+
+					System.out.println("The unicode for the character n is \\u006E"); 
+
+				}
+
+				else if (ch == 'o') {
+
+					System.out.println("The unicode for the character o is \\u006F"); 
+
+				}
+
+				else if (ch == 'p') {
+
+					System.out.println("The unicode for the character p is \\u0070"); 
+
+				}
+
+				else if (ch == 'q') {
+
+					System.out.println("The unicode for the character q is \\u0071"); 
+
+				}
+
+				else if (ch == 'r') {
+
+					System.out.println("The unicode for the character r is \\u0072"); 
+
+				}
+				else if (ch == 's') {
+
+					System.out.println("The unicode for the character s is \\u0073"); 
+
+				}
+				else if (ch == 't') {
+
+					System.out.println("The unicode for the character t is \\u0074"); 
+
+				}
+				else if (ch == 'u') {
+
+					System.out.println("The unicode for the character u is \\u0075"); 
+
+				}
+				else if (ch == 'v') {
+
+					System.out.println("The unicode for the character v is \\u0076"); 
+
+				}
+				else if (ch == 'w') {
+
+					System.out.println("The unicode for the character w is \\u0077"); 
+
+				}
+				else if (ch == 'x') {
+
+					System.out.println("The unicode for the character x is \\u0078"); 
+
+				}
+				else if (ch == 'y') {
+
+					System.out.println("The unicode for the character y is \\u0079"); 
+
+				}
+				else if (ch == 'z') {
+
+					System.out.println("The unicode for the character z is \\u007A"); 
+
+				}
+				
+				
+				// A-Z
+
+			
+				
+				
+				else if (ch == 'A') {
+
+					System.out.println("The unicode for the character A is \\u0041"); 
+
+				}
+				else if (ch == 'B') {
+
+					System.out.println("The unicode for the character B is \\u0042"); 
+
+				}
+				else if (ch == 'C') {
+
+					System.out.println("The unicode for the character C is \\u0043"); 
+
+				}
+				else if (ch == 'D') {
+
+					System.out.println("The unicode for the character D is \\u0044"); 
+
+				}
+				else if (ch == 'E') {
+
+					System.out.println("The unicode for the character E is \\u0045"); 
+
+				}
+				else if (ch == 'F') {
+
+					System.out.println("The unicode for the character F is \\u0046"); 
+
+				}
+				else if (ch == 'G') {
+
+					System.out.println("The unicode for the character G is \\u0047"); 
+
+				}
+				else if (ch == 'H') {
+
+					System.out.println("The unicode for the character H is \\u0048"); 
+
+				}
+				else if (ch == 'H') {
+
+					System.out.println("The unicode for the character I is \\u0049"); 
+
+				}
+				else if (ch == 'J') {
+
+					System.out.println("The unicode for the character J is \\u004A"); 
+
+				}
+				else if (ch == 'K') {
+
+					System.out.println("The unicode for the character K is \\u004B"); 
+
+				}
+				else if (ch == 'L') {
+
+					System.out.println("The unicode for the character L is \\u004C"); 
+
+				}
+				else if (ch == 'M') {
+
+					System.out.println("The unicode for the character M is \\u004D"); 
+
+				}
+				else if (ch == 'N') {
+
+					System.out.println("The unicode for the character N is \\u004E"); 
+
+				}
+				else if (ch == 'O') {
+
+					System.out.println("The unicode for the character O is \\u004F"); 
+
+				}
+				else if (ch == 'P') {
+
+					System.out.println("The unicode for the character P is \\u0050"); 
+
+				}
+				else if (ch == 'Q') {
+
+					System.out.println("The unicode for the character Q is \\u0051"); 
+
+				}
+				else if (ch == 'R') {
+
+					System.out.println("The unicode for the character R is \\u0052"); 
+
+				}
+				else if (ch == 'S') {
+
+					System.out.println("The unicode for the character S is \\u0053"); 
+
+				}
+				else if (ch == 'T') {
+
+					System.out.println("The unicode for the character T is \\u0054"); 
+
+				}
+				else if (ch == 'U') {
+
+					System.out.println("The unicode for the character U is \\u0055"); 
+
+				}
+				else if (ch == 'V') {
+
+					System.out.println("The unicode for the character V is \\u0056"); 
+
+				}
+				else if (ch == 'W') {
+
+					System.out.println("The unicode for the character W is \\u0057"); 
+
+				}
+				else if (ch == 'X') {
+
+					System.out.println("The unicode for the character X is \\u0058"); 
+
+				}
+				else if (ch == 'Y') {
+
+					System.out.println("The unicode for the character Y is \\u0059"); 
+
+				}
+				else if (ch == 'Z') {
+
+					System.out.println("The unicode for the character Z is \\u005A"); 
+
+				}
+				
+				//Symbols
+				
+				else if (ch == '!') {
+
+					System.out.println("The unicode for the character ! is \\u0021"); 
+
+				}
+				else if (ch == '\"') {
+
+					System.out.println("The unicode for the character " + "\"" + " is \\u0022");
+				}
+				else if (ch == '#') {
+
+					System.out.println("The unicode for the character # is \\u0023"); 
+
+				}
+				else if (ch == '$') {
+
+					System.out.println("The unicode for the character $ is \\u0024"); 
+
+				}
+				else if (ch == '%') {
+
+					System.out.println("The unicode for the character % is \\u0025"); 
+
+				}
+				else if (ch == '&') {
+
+					System.out.println("The unicode for the character & is \\u0026"); 
+
+				}
+				else if (ch == '\'') {
+
+					System.out.println("The unicode for the character \\ is \\u0027"); 
+
+				}
+				else if (ch == '(') {
+
+					System.out.println("The unicode for the character ( is \\u0028"); 
+
+				}
+				else if (ch == ')') {
+
+					System.out.println("The unicode for the character ) is \\u0029"); 
+
+				}
+				else if (ch == '*') {
+
+					System.out.println("The unicode for the character * is \\u002A"); 
+
+				}
+				else if (ch == '+') {
+
+					System.out.println("The unicode for the character + is \\u002B"); 
+
+				}
+				else if (ch == ',') {
+
+					System.out.println("The unicode for the character , is \\u002C"); 
+
+				}
+				else if (ch == '-') {
+
+					System.out.println("The unicode for the character - is \\u002D"); 
+
+				}
+				else if (ch == '.') {
+
+					System.out.println("The unicode for the character . is \\u002E"); 
+
+				}
+				else if (ch == '/') {
+
+					System.out.println("The unicode for the character / is \\u002F"); 
+
+				}
+				else if (ch == ':') {
+
+					System.out.println("The unicode for the character : is \\u003A"); 
+
+				}
+				else if (ch == ';') {
+
+					System.out.println("The unicode for the character ; is \\u003B"); 
+
+				}
+				else if (ch == '<') {
+
+					System.out.println("The unicode for the character < is \\u003C"); 
+
+				}
+				else if (ch == '=') {
+
+					System.out.println("The unicode for the character = is \\u003D"); 
+
+				}
+				else if (ch == '>') {
+
+					System.out.println("The unicode for the character > is \\u003E"); 
+
+				}
+				else if (ch == '?') {
+
+					System.out.println("The unicode for the character ? is \\u003F"); 
+
+				}
+				else if (ch == '@') {
+
+					System.out.println("The unicode for the character @ is \\u0040"); 
+
+				}
+				else if (ch == '[') {
+
+					System.out.println("The unicode for the character [ is \\u005B"); 
+
+				}
+				else if (ch == '\\') {
+
+					System.out.println("The unicode for the character \\ is \\u005C"); 
+
+				}
+				else if (ch == ']') {
+
+					System.out.println("The unicode for the character ] is \\u005D"); 
+
+				}
+				else if (ch == '^') {
+
+					System.out.println("The unicode for the character ^ is \\u005E"); 
+
+				}
+				else if (ch == '_') {
+
+					System.out.println("The unicode for the character _ is \\u005F"); 
+
+				}
+				else if (ch == '`') {
+
+					System.out.println("The unicode for the character ` is \\u0060"); 
+
+				}
+				else if (ch == '{') {
+
+					System.out.println("The unicode for the character { is \\u007B"); 
+
+				}
+				else if (ch == '|') {
+
+					System.out.println("The unicode for the character | is \\u007C"); 
+
+				}
+				else if (ch == '}') {
+
+					System.out.println("The unicode for the character } is \\u007D"); 
+
+				}
+				else if (ch == '~') {
+
+					System.out.println("The unicode for the character ~ is \\u007E"); 
+
+				}
+				else {
+					System.out.println("Whatever you have entered was invalid. You must enter a single character!");
+				}
+				
+				
+				
+
+				/*
+					 (space) = \u0020
+					 ! = \u0021
+					 " = \u0022
+					 # = \u0023
+					 $ = \u0024
+					 % = \u0025
+					 & = \u0026
+					 ' = \u0027
+					 ( = \u0028
+					 ) = \u0029 
+					 * = \u002A
+					 + = \u002B
+					 , = \u002C
+					 - = \u002D
+					 . = \u002E
+					 / = \u002F
+					 : = \u003A
+					 ; = \u003B
+					 < = \u003C
+					 = = \u003D
+					 > = \u003E
+					 ? = \u003F
+					 @ = \u0040
+					 [ = \u005B
+					 \ = \u005C 
+					 ] = \u005D
+					 ^ = \u005E
+					 _ = \u005F 
+					 ` = \u0060 
+					 { = \u007B
+					 | = \u007C
+					 } = \u007D
+					 ~ = \u007E
+					 
+					 
+					 
+				
+				 */
+
+				
+				
+				
+				
+				
+				
+				
+				
+	}
+
+}
